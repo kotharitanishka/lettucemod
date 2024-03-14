dba5b4a5c6b61c2b8457d710408d3f913aa615d1
tanishka <tanishka.k.kothari@gmail.com>
2024-03-14 23:30:26 +0530

    versioning done

--- src/main/java/com/example/lettucemoddemo/controller/Controller.java
+++ src/main/java/com/example/lettucemoddemo/controller/Controller.java
@@ -2,7 +2,6 @@ package com.example.lettucemoddemo.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.lettucemoddemo.model.Person;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redis.lettucemod.RedisModulesClient;
 import com.redis.lettucemod.api.StatefulRedisModulesConnection;
@@ -15,11 +14,7 @@ import com.redis.lettucemod.search.SearchResults;
 import io.lettuce.core.RedisURI;
 
 import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Set;
-
 import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -43,41 +38,21 @@ public class Controller {
     //Obtain the command API for synchronous execution.
     RedisModulesCommands<String, String> commands = connection.sync();
 
-
-    @GetMapping("/getById")
-    public String getPersonById(@RequestParam String id) {
-        String hkey = "version:People:" + id;
-        String ver = commands.hget(hkey, "v");
-        String key_p = "People:" + id + ":" + ver;
-        String person = commands.jsonGet(key_p, "$");
-        if (person == null ) {
-            return "id not found";
-        } 
-        else {
-            try {
-                JSONArray array = new JSONArray(person);
-                JSONObject object = array.getJSONObject(0); 
-                Person p = new ObjectMapper().readValue(object.toString(), Person.class);
-                if (p.isActive()) {
-                    return person;
-                }
-                else {
-                    return "user is deleted";
-                }
-                } catch (Exception e) {
-                    System.out.println(e);
-                return "error";
-            }
-            
-        }
-       
-    }
-
     @PostMapping("/new")
     public String addPerson(@RequestBody Person p, @RequestParam String user) {
         String p_json;
         if (p.getId() == null) {
-            return "cannot create with id";
+            return "cannot create without id";
+        }
+        try {
+            CreateOptions<String, String> options = CreateOptions.<String, String>builder()
+                                                     .on(CreateOptions.DataType.JSON)
+                                                     .prefixes("People:")
+                                                     .build();
+            commands.ftCreate("pix", options, Field.text("$.id").as("id").build() ,Field.text("$.name").as("name").build(), Field.numeric("$.age").as("age").build(), Field.tag("$.active").as("active").build());
+            //commands.ftCreate("p", options , Field.text(null).)
+        } catch (Exception e) {
+            System.out.println("already exists index");
         }
         try {
             
@@ -102,56 +77,15 @@ public class Controller {
         }  
     }
 
-    @GetMapping("/getByName")
-    public String getPersonByName(@RequestParam String n) {
-        try {
-            CreateOptions<String, String> options = CreateOptions.<String, String>builder()
-                                                     .on(CreateOptions.DataType.JSON)
-                                                     .prefix("People:")
-                                                     .build();
-            commands.ftCreate("pinx", options, Field.text("$.name").build(), Field.numeric("$.age").build());
-        } catch (Exception e) {
-            System.out.println("already exists index");
-        }
-        SearchResults<String , String> s = commands.ftSearch("pinx", n);
-        System.out.println(s);
-        if (s.isEmpty() == true) {
-            return "name not found";
-        } else {
-            return s.toString();
-        } 
-    }
-
-    @DeleteMapping("/deleteById")
-    public String deletePersonById(@RequestParam String id) {
-        
-        String hkey = "version:People:" + id;
-        String ver = commands.hget(hkey, "v");
-        if (ver == null) {
-            return "enter valid id";
-        }
-        String key_p = "People:" + id + ":" + ver;
-        commands.jsonSet(key_p , "$.active" , "false");
-        return "Deleted People : " + id;
-    }
-
-    // @PutMapping("/NameById")
-    // public String updatePersonNameById(@RequestParam String id , @RequestBody(required = false) String n) {
-    //     String key_p = "People:" + id ;
-    //     //System.out.println(n);
-    //     try {
-    //         String updatedPerson = commands.jsonSet(key_p, "$.name", n , SetMode.XX);
-    //         System.out.println(updatedPerson);
-    //         return "updated";
-    //     } catch (Exception e) {
-    //         return "error , did not update";
-    //     }
-        
-    // }
 
     @GetMapping("/getAll")
-    public String getAllPerson() {
-        SearchResults<String,String> ans = commands.ftSearch("pinx", "*");
+    public String getAllPerson(@RequestParam(required = false) Boolean inact) {
+        SearchResults<String,String> ans;
+        if (inact == null || inact == false )  {
+            ans = commands.ftSearch("pidx", "@active:{true}");
+        } else {
+            ans = commands.ftSearch("pidx", "*");
+        }
         if (ans.isEmpty() == true) {
             return "empty";
         } else {
@@ -159,52 +93,65 @@ public class Controller {
         } 
     }
 
-    @PutMapping("/updateById")
-    public String updatePersonById(@RequestParam String id ,@RequestParam String user ,@RequestBody(required = false) Map<String,Object> m) {
+
+    @GetMapping("/getById")
+    public String getPersonById(@RequestParam String id , @RequestParam(required = false) Boolean inact) {
         String hkey = "version:People:" + id;
         String ver = commands.hget(hkey, "v");
         String key_p = "People:" + id + ":" + ver;
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        String time = timestamp.toString();
-        try {
-            String t = new ObjectMapper().writeValueAsString(time);
-            commands.jsonSet(key_p, "$.updatedOn" ,t , SetMode.XX);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            System.out.println(e);
-        }
-
-        Set<String> p_keys = new LinkedHashSet<String>();   
-        p_keys.add("name");
-        p_keys.add("age");
-        //p_keys.add("updatedBy");
-        Object[] keys = m.keySet().toArray();
-        try {
-            for (int i = 0 ; i < keys.length ; i ++) {
-                String val = new ObjectMapper().writeValueAsString(m.get(keys[i]));
-                System.out.println(val);
-                if (p_keys.contains(keys[i])) {
-                    String path = "$." + keys[i];
-                    commands.jsonSet(key_p,path ,val , SetMode.XX);
-                }
-                else if (keys[i] == "id") {
-                        System.out.println("cannot change key");
-                }
+        String person = commands.jsonGet(key_p, "$");
+        if (person == null ) {
+            return "id not found";
+        } 
+        else {
+            try {
+                JSONArray array = new JSONArray(person);
+                JSONObject object = array.getJSONObject(0); 
+                Person p = new ObjectMapper().readValue(object.toString(), Person.class);
+                if (p.isActive()) {
+                    if (inact == null || inact == false) {
+                        return person;
+                    }
+                    else {
+                        String q = "@id:" + id ;
+                        return commands.ftSearch("pix", q).toString();
+                    }
+                } 
                 else {
-                        String path = "$.detail." + keys[i];
-                        commands.jsonSet(key_p,path ,val);
+                    return "user is deleted";
                 }
-                }
-                String u = new ObjectMapper().writeValueAsString(user);
-                commands.jsonSet(key_p, "$.updatedBy" ,u , SetMode.XX);
-                return "updated";
-            } 
-        catch (Exception e) {
-            System.out.println(e);
-            return "error , did not update";
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                return "error";
+            }
+            
         }
+       
     }
-    
+ 
+
+    @GetMapping("/getByName")
+    public String getPersonByName(@RequestParam String n, @RequestParam(required = false) Boolean inact) {
+        
+        String q;
+        if (inact == null || inact == false) {
+            q = "(@name:" + n + " & @active:{true})" ;
+        }
+        else {
+            q = "@name:" + n ;
+        }
+        SearchResults<String , String> s = commands.ftSearch("pidx", q);
+        System.out.println(n);
+        System.out.println(s);
+        if (s.isEmpty() == true) {
+            return "name not found";
+        }
+        else{
+            return s.toString();
+        } 
+    }
+
 
     @PutMapping("/updateEntireById")
     public String updatePersonFullById(@RequestParam String id ,@RequestParam String user ,@RequestBody(required = false) Map<String,Object> m) {
@@ -257,7 +204,19 @@ public class Controller {
         }   
     }
 
-
+    
+    @DeleteMapping("/deleteById")
+    public String deletePersonById(@RequestParam String id) {
+        
+        String hkey = "version:People:" + id;
+        String ver = commands.hget(hkey, "v");
+        if (ver == null) {
+            return "enter valid id";
+        }
+        String key_p = "People:" + id + ":" + ver;
+        commands.jsonSet(key_p , "$.active" , "false");
+        return "Deleted People : " + id;
+    }
     
     
 }
